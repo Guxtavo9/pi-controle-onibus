252edaa359f4c5b023730fe28808ffd080952629
Gustavo~ <>
2023-12-05 13:19:59 -0300

    concertando pequenas coisas

--- frontend/routes/publicRouter.js
+++ frontend/routes/publicRouter.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 //rota INDEX
 
-router.get("/public-site", function (req, res, next) {
+router.get("/", function (req, res, next) {
   res.render("public-site/index");
 });
 
